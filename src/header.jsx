597eff04da8167235bbbd942ccf72576cfcd84e3
TechWhiz0 <educationalgamerindia@gmail.com>
2025-02-24 10:58:26 +0530

    file added

---
+++ src/header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <header className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-6 shadow-lg">
+          <div className="max-w-4xl mx-auto flex items-center justify-center">
+            <h1 className="text-3xl font-bold text-white tracking-tight">
+              QuickBite
+            </h1>
+          </div>
+        </header>
+      );
+  }
+  
